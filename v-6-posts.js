410ed68caef0ddeed86fa08fedd24a51c8d74cef
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-13 22:44:20 +0530

    added v-6

---
+++ v-6-posts.js
@@ -0,0 +1,24 @@
+// json data load 
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => displayPosts(data))
+}
+loadPosts();
+
+function  displayPosts(posts){
+    const postsContainer = document.getElementById("posts")
+    for(const post of posts){
+        const div = document.createElement("div");
+        // in here added  class ="posts"
+        div.classList.add("posts")
+        // set in html tag
+        div.innerHTML = `<h2>${post.title}</h2>, <p>${post.id}</p>,
+        <small>${post.body}</small>` ;
+        // appendChild
+        postsContainer.appendChild(div);
+        // console.log(post.id)
+    }
+    // console.log(posts)
+}
+// console.log("hello this is v-6")
