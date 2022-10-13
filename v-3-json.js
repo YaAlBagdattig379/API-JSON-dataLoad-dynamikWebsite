a6329505ba9cc691f0e2b64f443da1fd21fb4d88
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-13 12:00:47 +0530

    added

--- v-3-json.js
+++ v-3-json.js
@@ -6,4 +6,25 @@
         weight : 70,
         isMarried : true,  
         books : ["english","bengali","finish"]   
-}
+};
+// use for 'stringify'
+const superStrigified = JSON.stringify(headTeacher);
+// console.log(superStrigified); 
+    // o.p {"friendName":"anisul","age":31,"height":" 6 feet ","weight":70,"isMarried":true,"books":["english","bengali","finish"]}
+
+// use for 'parse'  to converted
+const converted = JSON.parse(superStrigified);
+console.log(converted);
+ /* 
+     o.p >>      
+    {
+    friendName: 'anisul',
+    age: 31,
+    height: ' 6 feet ',
+    weight: 70,
+    isMarried: true,
+    books: [ 'english', 'bengali', 'finish' ]
+  } 
+   
+  */
+ 
