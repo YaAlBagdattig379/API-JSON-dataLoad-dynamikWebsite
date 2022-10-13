2a33fdfb012c8707321fbe36534a6d680fe46766
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-13 11:36:39 +0530

    first commit

---
+++ v-3-json.js
@@ -0,0 +1,9 @@
+
+    headTeacher = {
+        friendName : "anisul",
+        age : 31,
+        height : " 6 feet ",
+        weight : 70,
+        isMarried : true,  
+        books : ["english","bengali","finish"]   
+}
