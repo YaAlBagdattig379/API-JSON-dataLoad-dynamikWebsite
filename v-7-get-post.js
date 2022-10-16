c4c3bd37a108a4624d765c755fde8d217ebe79e6
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-16 20:25:09 +0530

    added

--- v-7-get-post.js
+++ v-7-get-post.js
@@ -1,42 +1,21 @@
 // console.log("hello get and post two things is more required for both of us.")
 
-// json data load 
-/*
-function apiPosts(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      method : 23,
+// json data 'post'............this is example of 'post' how to   write post?
+
+function addAPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+      method : "post",
       body : JSON.stringify({
-        title : 'document.get,
-        body : "do",
-        userId : 22;
-        " 
-      });
+        title : "my new pow",
+        body : "this is my posts body !",
+        id : 4
+      }),
+      headers : {
+        "content-type" : 'application/json; charset = UTF'
+      },
 
-      .then
-      .then
+    })
+    .then( res => res.json())
+    .then( data => console.log(data))
+   
 }
-loadPosts();
-
-function  displayPosts(posts){
-    const postsContainer = document.getElementById("posts")
-    for(const post of posts){
-        const div = document.createElement("div");
-        // in here added  class ="posts"
-        div.classList.add("posts")
-        // set in html tag
-        div.innerHTML = `<h2>${post.title}</h2>, <p>${post.id}</p>,
-        <small>${post.body}</small>` ;
-        // appendChild
-        postsContainer.appendChild(div);
-        // console.log(post.id)
-    }
-    // console.log(posts)
-}
-// console.log("hello this is v-6") */
-
-
-/*
-
-console.log("hello this is v-6",20022)
-
-*/
